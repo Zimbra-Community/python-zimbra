5712fa0bdb7fc247c8176d5520cf841dd6534ab7
Dennis Ploeger <develop@dieploegers.de>
2014-12-23 13:38:57 +0100

    * Added an xml-variant of the gen_request_batch-test and added a test wether the original gen_request_batch-test generates a JSON request

--- tests/test_genrequest.py
+++ tests/test_genrequest.py
@@ -1,6 +1,8 @@
 """ Test usage of communication.authenticate and communication.gen_request
 """
 from unittest import TestCase
+from pythonzimbra.request_json import RequestJson
+from pythonzimbra.request_xml import RequestXml
 from pythonzimbra.tools.auth import authenticate
 from pythonzimbra.communication import Communication
 from tests import get_config
@@ -132,6 +134,103 @@ class TestGenrequest(TestCase):
             )
 
             request = comm.gen_request(token=token, set_batch=True)
+
+            self.assertEqual(
+                type(request),
+                RequestJson,
+                "Generated request wasn't an json-request, which should be "
+                "the default."
+            )
+
+            request.add_request(
+                "NoOpRequest",
+                {
+
+                },
+                "urn:zimbraMail"
+            )
+
+            request.add_request(
+                "NoOpRequest",
+                {
+
+                },
+                "urn:zimbraMail"
+            )
+
+            response = comm.send_request(request)
+
+            if response.is_fault():
+
+                self.fail(
+                    "Reponse failed: (%s) %s" % (
+                        response.get_fault_code(),
+                        response.get_fault_message()
+                    )
+                )
+
+            self.assertEqual(
+                response.is_batch(),
+                True,
+                "Batch-request didn't return a Batch response."
+            )
+
+            expected_batch = {
+                'nameToId': {
+                    'NoOpResponse': [
+                        '1',
+                        '2'
+                    ]
+                },
+                'hasFault': False,
+                'idToName': {
+                    '1': 'NoOpResponse',
+                    '2': 'NoOpResponse'
+                }
+            }
+
+            self.assertEqual(
+                response.get_batch(),
+                expected_batch,
+                "Batch-dictionary wasn't expected"
+            )
+
+    def test_genrequest_batch_xml(self):
+
+        """ Create a request only using the Communication-object (xml-version)
+        """
+
+        config = get_config()
+
+        if config.getboolean("genrequest_test", "enabled"):
+
+            # Run only if enabled
+
+            comm = Communication(config.get("genrequest_test", "url"))
+
+            token = authenticate(
+                config.get("genrequest_test", "url"),
+                config.get("genrequest_test", "account"),
+                config.get("genrequest_test", "preauthkey")
+            )
+
+            self.assertNotEqual(
+                token,
+                None,
+                "Cannot authenticate."
+            )
+
+            request = comm.gen_request(
+                request_type="xml",
+                token=token,
+                set_batch=True
+            )
+
+            self.assertEqual(
+                type(request),
+                RequestXml,
+                "Generated request wasn't an xml-request"
+            )
 
             request.add_request(
                 "NoOpRequest",
