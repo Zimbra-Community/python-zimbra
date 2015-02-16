522469a28ac328ee044e48392b75c1494c5fda3e
Dennis Ploeger <develop@dieploegers.de>
2015-02-16 13:30:37 +0100

    v2.0 for pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='python-zimbra',
-    version='2.0rc1',
+    version='2.0',
     packages=['pythonzimbra'],
     include_package_data=True,
     license='BSD 2-clause License',  # example license
