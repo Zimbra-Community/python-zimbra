f7e518bdf5ed25574eb7fe6717b0de0671900ec7
Barry de Graaff <info@barrydegraaff.tk>
2021-05-24 19:52:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='python-zimbra',
-    version='2.1',
+    version='2.3',
     packages=['pythonzimbra'],
     include_package_data=True,
     license='BSD 2-clause License',  # example license
